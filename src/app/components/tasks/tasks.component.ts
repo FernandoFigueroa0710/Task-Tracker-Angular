75171b8802e28c427fd8848d1bb2924260bf688a
Fernando A Figueroa <fernando.figueroa@corcoran.com>
2022-11-26 16:24:52 -0600

    connected fake tasks to the main component

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+import { Task } from 'src/app/Models/Task';
+import { TASKS } from 'src/app/mock-tasks';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent {
-
+  tasks: Task[] = TASKS;
 }
